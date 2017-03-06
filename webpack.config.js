26f530541d8d53431039a1f0d40713ea7f2d3e64
git <admin@lianyue.org>
2017-03-06 20:28:03 +0900

    Update devtool

--- webpack.config.js
+++ webpack.config.js
@@ -59,11 +59,11 @@ module.exports = {
     historyApiFallback: true,
     noInfo: true
   },
-  devtool: '#eval-source-map'
+  devtool: 'eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = 'source-map'
   // http://vuejs.github.io/vue-loader/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
