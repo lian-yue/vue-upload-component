a61c424d998c2fea7aa7e87b546c09d5ceb8081a
Johannes Noe <docnoe@users.noreply.github.com>
2017-04-05 10:47:49 +0200

    Switching from render function to template

--- src/InputFile.vue
+++ src/InputFile.vue
@@ -1,24 +1,20 @@
+<template>
+  <input
+    type="file"
+    name="$parent.name"
+    id="$parent.id || $parent.name"
+    accept="$parent.accept"
+    on-change="change"
+    multiple="$parent.multiple && $parent.mode == 'html5'"
+  />
+</template>
 <script>
 export default {
   methods: {
     change(e) {
-      this.$destroy();
-      this.$parent._addInputFileElement(e.target);
-    },
-  },
-
-  render(h) {
-    const parent = this.$parent;
-    return (
-      <input
-        type="file"
-        name={parent.name}
-        id={parent.id|| parent.name}
-        accept={parent.accept}
-        on-change={this.change}
-        multiple={parent.multiple && parent.mode == 'html5'}
-      />
-    )
+      this.$destroy()
+      this.$parent._addInputFileElement(e.target)
+    }
   }
 }
 </script>
