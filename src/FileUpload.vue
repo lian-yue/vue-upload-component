1d0e93a23ed69fdee0254bab7c00c62796b14c52
git <admin@lianyue.org>
2017-11-30 22:20:21 +0900

    fix #120

--- src/FileUpload.vue
+++ src/FileUpload.vue
@@ -975,6 +975,7 @@ export default {
           input = document.createElement('input')
           input.type = 'hidden'
           input.name = key
+          input.value = value
           form.appendChild(input)
         }
       }
